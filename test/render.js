74dd5626f06d438e68fb71320a2aedc9ae52eb4e
Luiz Américo <brandkarl@gmail.com>
2016-11-02 13:10:41 -0300

    Add tests to rendering views

---
+++ test/render.js
@@ -0,0 +1,148 @@
+import chai from 'chai';
+import jsdom from 'mocha-jsdom';
+import sinonChai from 'sinon-chai';
+import {Route, createRouter, destroyRouter, middleware} from '../src/index';
+import Mn from 'backbone.marionette';
+import Backbone from 'backbone';
+
+let expect = chai.expect;
+chai.use(sinonChai);
+
+
+let router, routes;
+let RootRoute, ParentRoute, ChildRoute, GrandChildRoute, LeafRoute;
+
+let ParentView = Mn.View.extend({
+  template: function () {
+    return '<div class="child-view"></div>'
+  },
+  regions: {
+    outlet: '.child-view'
+  }
+});
+
+let GrandChildView = Mn.View.extend({
+  tagName: 'h2',
+  template: function () {
+    return 'GrandChild'
+  }
+});
+
+let LeafView = Mn.View.extend({
+  template: function () {
+    return 'Leaf'
+  }
+});
+
+describe('Render', () => {
+
+  beforeEach(() => {
+    router = createRouter({location: 'memory'});
+    router.use(middleware);
+    ParentRoute = Route.extend({
+      viewClass: ParentView
+    });
+    RootRoute = Route.extend({}), ChildRoute = Route.extend({}),
+      GrandChildRoute = Route.extend({}), LeafRoute = Route.extend({});
+    routes = function (route) {
+      route('parent', {routeClass: ParentRoute}, function () {
+        route('child', {routeClass: ChildRoute}, function () {
+          route('grandchild', {viewClass: GrandChildView}, function () {
+            route('leaf', {routeClass: LeafRoute, viewClass: LeafView})
+          })
+        })
+      });
+      route('root', {routeClass: RootRoute, routeOptions: {viewClass: ParentView}});
+      route('root2', {viewClass: ParentView})
+    };
+    router.map(routes);
+    router.listen();
+  });
+
+  afterEach(() => {
+    destroyRouter(router);
+  });
+
+  describe('viewClass', function () {
+    let $;
+    jsdom();
+
+    before(function () {
+      Backbone.$ = $ = require('jquery')(window)
+    });
+
+    beforeEach(function () {
+      document.body.innerHTML = '<div id="main"></div>';
+      let RootRegion = Mn.Region.extend({
+        el: '#main'
+      });
+      router.rootRegion = new RootRegion()
+    });
+
+    it('can be defined in the Route class', function(done){
+      router.transitionTo('parent').then(function () {
+        expect($('#main').html()).to.be.equal('<div><div class="child-view"></div></div>');
+        done()
+      }).catch(done)
+    });
+
+    it('can be passed through routeOptions.viewClass', function(done){
+      router.transitionTo('root').then(function () {
+        expect($('#main').html()).to.be.equal('<div><div class="child-view"></div></div>');
+        done()
+      }).catch(done)
+    });
+
+    it('can be passed through viewClass, without a routeClass', function(done){
+      router.transitionTo('root2').then(function () {
+        expect($('#main').html()).to.be.equal('<div><div class="child-view"></div></div>');
+        done()
+      }).catch(done)
+    });
+
+    describe('of a root route', function () {
+
+      it('should be rendered in rootRegion', function (done) {
+        router.transitionTo('parent').then(function () {
+          expect($('#main').html()).to.be.equal('<div><div class="child-view"></div></div>');
+          done()
+        }).catch(done)
+      });
+
+      it('should abort transition if no rootRegion is defined', function (done) {
+        router.rootRegion = null;
+        router.transitionTo('parent').then(function () {
+          done('transition resolved')
+        }).catch(function (error) {
+          expect(error).to.be.an('error');
+          expect(error.message).to.be.equal('No outlet region');
+          done()
+        })
+      })
+    });
+
+    describe('of a child route', function () {
+
+      it('should be rendered in the outlet region of the nearest route with a view', function (done) {
+        router.transitionTo('grandchild').then(function () {
+          expect($('#main').html()).to.be.equal('<div><div class="child-view"><h2>GrandChild</h2></div></div>');
+          done()
+        }).catch(done)
+      });
+
+      it('should abort transition if no outlet region is defined in the nearest route with a view', function (done) {
+        router.transitionTo('leaf').then(function () {
+          done('transition resolved')
+        }).catch(function (error) {
+          expect(error).to.be.an('error');
+          expect(error.message).to.be.equal('No outlet region in view');
+          done()
+        })
+      })
+
+    })
+
+  })
+
+});
+  
