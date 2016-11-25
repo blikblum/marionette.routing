99daa1ec3104c5c7b6d3c6374e1b5f643c4f66e9
Luiz Américo <luizamericop@gmail.com>
2016-11-25 11:06:24 -0300

    Rewrite RouteContext as a function constructor instead of a class

--- src/routecontext.js
+++ src/routecontext.js
@@ -1,26 +1,26 @@
-export default class RouteContext {
-  constructor(routes, route) {
-    let routeIndex = routes.indexOf(route)
-    this.parentRoutes = routes.slice(0, routeIndex)
-  }
+function RouteContext(routes, route) {
+  let routeIndex = routes.indexOf(route)
+  this.parentRoutes = routes.slice(0, routeIndex)
+}
 
-  trigger() {
-    let parentRoutes = this.parentRoutes
-    for (let i = parentRoutes.length - 1; i >= 0; i--) {
-      let channel = parentRoutes[i]._contextChannel
-      if (channel) {
-        channel.trigger.apply(channel, arguments)
-      }
-    }
-  }
-
-  request(name) {
-    let parentRoutes = this.parentRoutes
-    for (let i = parentRoutes.length - 1; i >= 0; i--) {
-      let channel = parentRoutes[i]._contextChannel
-      if (channel && channel._requests[name]) {
-        return channel.request.apply(channel, arguments)
-      }
+RouteContext.prototype.trigger = function() {
+  let parentRoutes = this.parentRoutes
+  for (let i = parentRoutes.length - 1; i >= 0; i--) {
+    let channel = parentRoutes[i]._contextChannel
+    if (channel) {
+      channel.trigger.apply(channel, arguments)
     }
   }
 }
+
+RouteContext.prototype.request = function(name) {
+  let parentRoutes = this.parentRoutes
+  for (let i = parentRoutes.length - 1; i >= 0; i--) {
+    let channel = parentRoutes[i]._contextChannel
+    if (channel && channel._requests[name]) {
+      return channel.request.apply(channel, arguments)
+    }
+  }
+}
+
+export default RouteContext
