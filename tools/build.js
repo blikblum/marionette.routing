1f8ddb2126358e517e49d77c648e2ea75dca7afd
Luiz Américo <luizamericop@gmail.com>
2018-09-04 20:08:37 -0300

    Update dependencies and change build file name to marionette.routing

--- tools/build.js
+++ tools/build.js
@@ -1,15 +1,15 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const del = require('del');
-const rollup = require('rollup');
-const babel = require('rollup-plugin-babel');
-const pkg = require('../package.json');
+const fs = require('fs')
+const del = require('del')
+const rollup = require('rollup')
+const babel = require('rollup-plugin-babel')
+const pkg = require('../package.json')
 
-let promise = Promise.resolve();
+let promise = Promise.resolve()
 
 // Clean up the output directory
-promise = promise.then(() => del(['dist/*']));
+promise = promise.then(() => del(['dist/*']))
 
 // Compile source code into a distributable format with Babel
 for (const format of ['es', 'umd']) {
@@ -19,10 +19,10 @@ for (const format of ['es', 'umd']) {
     plugins: [babel(Object.assign(pkg.babel, {
       babelrc: false,
       exclude: 'node_modules/**',
-      presets: pkg.babel.presets.map(x => (x === 'latest' ? ['latest', { es2015: { modules: false } }] : x)),
-    }))],
+      presets: pkg.babel.presets.map(x => (x === 'latest' ? ['latest', { es2015: { modules: false } }] : x))
+    }))]
   }).then(bundle => bundle.write({
-    dest: `dist/${format === 'umd' ? 'index' : 'index.esm'}.js`,
+    dest: `dist/${format === 'umd' ? 'marionette.routing' : 'marionette.routing.esm'}.js`,
     format,
     sourceMap: true,
     moduleName: format === 'umd' ? 'Marionette.Routing' : undefined,
@@ -32,20 +32,23 @@ for (const format of ['es', 'umd']) {
       'backbone.marionette': 'Backbone.Marionette',
       'backbone.radio': 'Backbone.Radio'
     }
-  })));
+  })))
 }
 
 // Copy package.json and LICENSE.txt
 promise = promise.then(() => {
-  delete pkg.private;
-  delete pkg.devDependencies;
-  delete pkg.scripts;
-  delete pkg.eslintConfig;
-  delete pkg.babel;
-  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8');
-  fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8');
-  fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8');
-  fs.writeFileSync('dist/CHANGELOG.md', fs.readFileSync('CHANGELOG.md', 'utf-8'), 'utf-8');
-});
+  delete pkg.private
+  delete pkg.devDependencies
+  delete pkg.scripts
+  delete pkg.eslintConfig
+  delete pkg.babel
+  fs.writeFileSync('dist/package.json', JSON.stringify(pkg, null, '  '), 'utf-8')
+  fs.writeFileSync('dist/LICENSE.txt', fs.readFileSync('LICENSE.txt', 'utf-8'), 'utf-8')
+  fs.writeFileSync('dist/README.md', fs.readFileSync('README.md', 'utf-8'), 'utf-8')
+  fs.writeFileSync('dist/CHANGELOG.md', fs.readFileSync('CHANGELOG.md', 'utf-8'), 'utf-8')
+})
 
-promise.catch(err => console.error(err.stack)); // eslint-disable-line no-console
+promise.catch(err => {
+  console.error(err.stack) // eslint-disable-line no-console
+  process.exit(1)
+})
